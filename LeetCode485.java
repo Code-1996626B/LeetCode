1b3d3e73481b08394ac08335f31635b436303829
GitNoob <55232990+YunFeiBi@users.noreply.github.com>
2021-02-16 12:35:17 +0800

    2021-02-16

--- LeetCode485.java
+++ LeetCode485.java
@@ -1,4 +1,26 @@
 package LeetCode;
 
 public class LeetCode485 {
+
+    public static void main(String[] args) {
+        int arr[] = {1, 1, 0, 1, 1, 1};
+        System.out.println(findMaxConsecutiveOnes(arr));
+    }
+
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int maxLen = 0, len = 0;
+        boolean flag = false;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                len++;
+            } else {
+                len = 0;
+            }
+
+            if (len > maxLen) {
+                maxLen = len;
+            }
+        }
+        return maxLen;
+    }
 }
---
+++ LeetCode561.java
@@ -0,0 +1,20 @@
+package LeetCode;
+
+import java.util.Arrays;
+
+public class LeetCode561 {
+
+    public static void main(String[] args) {
+        int arr[] = {6,2,6,5,1,2};
+        System.out.println(arrayPairSum(arr));
+    }
+
+    public static int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int maxSum = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            maxSum += nums[i];
+        }
+        return maxSum;
+    }
+}
