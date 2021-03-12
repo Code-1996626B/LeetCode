83633ad2815f353826cf82f19b80dedb08a40d24
GitNoob <55232990+YunFeiBi@users.noreply.github.com>
2021-03-12 09:14:18 +0800

    2021-03-12

---
+++ LeetCode331.java
@@ -0,0 +1,37 @@
+package LeetCode;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class LeetCode331 {
+
+    public static void main(String[] args) {
+        String s = "1";
+        System.out.println(isValidSerialization(s));
+    }
+
+    public static boolean isValidSerialization(String preorder) {
+        int n = preorder.length();
+        int i = 0;
+        int slots = 1;
+        while (i < n) {
+            if (slots == 0) {
+                return false;
+            }
+            if (preorder.charAt(i) == ',') {
+                i++;
+            } else if (preorder.charAt(i) == '#'){
+                slots--;
+                i++;
+            } else {
+                // 读一个数字
+                while (i < n && preorder.charAt(i) != ',') {
+                    i++;
+                }
+                slots++; // slots = slots - 1 + 2
+            }
+        }
+        return slots == 0;
+    }
+
+}
