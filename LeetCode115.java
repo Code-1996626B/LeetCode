b48e79b9cdaa271c2c75308f508581c777d28505
GitNoob <55232990+YunFeiBi@users.noreply.github.com>
2021-03-18 09:52:26 +0800

    2021-03-17

---
+++ LeetCode115.java
@@ -0,0 +1,33 @@
+package LeetCode;
+
+public class LeetCode115 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int numDistinct(String s, String t) {
+        int m = s.length(), n = t.length();
+        if (m < n) {
+            return 0;
+        }
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            dp[i][n] = 1;
+        }
+
+        for (int i = m - 1; i >= 0; i--) {
+            char schar = s.charAt(i);
+            for (int j = n - 1; j >= 0; j--) {
+                char tchar = t.charAt(j);
+                if (schar == tchar) {
+                    dp[i][j] = dp[i + 1][j + 1] + dp[i + 1][j];
+                } else {
+                    dp[i][j] = dp[i + 1][j];
+                }
+            }
+        }
+        return dp[0][0];
+    }
+
+}
